4584b3eb3c62e95bdef179b49df258e56f91bce5
Jlynnraz <jlynnraz@gmail.com>
2019-09-20 14:42:23 -0600

    made google map changes

--- app.js
+++ app.js
@@ -5,23 +5,29 @@ var breweryTypeLoc = `/brewery/:breweryId/locations`;
 var beerByBrewery = `/beer/:beerId/breweries`;
 var locality = `/locations`;
 var endpoint = "brewery/:breweryId/locations";
-var map = function initMap(){
-    map = new google.maps.Map(document.getElementById('map'))
-
-    }
-initMap();
+var map;
+ function createMap(){
+     var options = {
+         center: {lat: 39.74, lng: 104.99},
+         zoom: 8
+     };
+     map = new google.maps.Map(document.getElementById("map"). options);
+    //  $("#map").append(map)
+     console.log(map)
+ }
+createMap()
 
 $("#brew-type-loc").on("click", function(event){
     event.preventDefault();
     var brewLoc = ("http://api.brewerydb.com/v2/" + endpoint + "/?key=94780a63bf05dcb19f858d5285c41fbb");
     
-    console.log(map)
+    
 
     $.ajax({
         url: brewLoc,
         method: "GET"
     }).then(function(response){
-        console.log("Click")
     })
+    console.log("Click")
   }
 );
