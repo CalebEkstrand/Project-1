bb3f58027c8154e06527f8f6976cc52bd63307ec
lambertco <blambertco@gmail.com>
2019-09-24 14:16:25 -0600

    nav bar redone, buttons still only work when they want to

--- app.js
+++ app.js
@@ -110,6 +110,33 @@ $("#btn").on("click", function (event) {
 
     });
 });
+
+var tabs = $('.tabs');
+var selector = $('.tabs').find('a').length;
+var selector = $(".tabs").find(".selector");
+var activeItem = tabs.find('.active');
+var activeWidth = activeItem.innerWidth();
+$(".selector").css({
+  "left": activeItem.position.left + "px", 
+  "width": activeWidth + "px"
+});
+
+$(".tabs").on("click","a",function(e){
+  e.preventDefault();
+  $('.tabs a').removeClass("active");
+  $(this).addClass('active');
+  var activeWidth = $(this).innerWidth();
+  var itemPos = $(this).position();
+  $(".selector").css({
+    "left":itemPos.left + "px", 
+    "width": activeWidth + "px"
+  });
+  var href = $(this).attr("data-href");
+  setTimeout(function(){ 
+      window.location.href = href;
+
+  },500);
+});
     //  createMap()
 
 
